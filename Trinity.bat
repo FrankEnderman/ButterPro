495d2534e2fb09709160d4431750a545bc4a8a3a
FrankEnderman <90335574+FrankEnderman@users.noreply.github.com>
2021-09-24 11:47:45 +0530

    added nuke icon when startup
     fixed grammar mistakes

--- Trinity.bat
+++ Trinity.bat
@@ -1,9 +1,67 @@
  @echo off
-
+set EMOTE == ":D"
 color 0a
 title Butter Trinity v4 SP4
+
 goto boot
 :boot
+cls
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@QX)E@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@E)XQ@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@E^`  'W@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@W'  `^E@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@G=      `k@@@@@@@@@@@@@@@@@@@@@@@@@@@@w`      =W@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@M!          L@@@@@@@@@@@@@@@@@@@@@@@@@@L          !M@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@Q*             r#@@@@@@@@@@@@@@@@@@@@@@#r             *Q@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@l`               _0@@@@@@@@@@@@@@@@@@@@0_               `u@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@#r                  .M@@@@@@@@@@@@@@@@@@M.                  r#@@@@@@@@@@@
+echo @@@@@@@@@@#^                    `j@@@@@@@@@@@@@@@@j`                    ^#@@@@@@@@@@
+echo @@@@@@@@@@)                       }@@@@@@@@@@@@@@}                       )@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@k                         r#@@@@@@@@@@#r                         k@@@@@@@@@
+echo @@@@@@@@Q-                         `j@@@@@@@@@@j`                         -Q@@@@@@@@
+echo @@@@@@@@T                        `}B@@@Qd3KdQ@@@B}`                        l@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@#:                       _D@@#V:      :V#@@E_                       :#@@@@@@@
+echo @@@@@@@g`                      `d@@Q:          :Q@@d`                      `g@@@@@@@
+echo @@@@@@@d                       ~@@@v            v@@@~                       d@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@v            v@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Q:          :Q@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#y:      :V#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Qd33dQ@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@0I0#@@@@#0I0@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@W'  `-,,-`  'W@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@k`            `k@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@L                L@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#r                  r#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@B=                    =B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@g,                      ,g@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@I`                        `X@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@}                            }@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@#)                              )#@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@#r                                r#@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@#du^-                        -^uO#@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@QZk]*!_'`      `'_!*]kZQ@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#BQQQQB#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 3 /NOBREAK >nul
 cls
 echo welcome!
 timeout /T 1 /NOBREAK >nul
@@ -164,25 +222,37 @@ if %input% == 1 goto menu
 :calc
 title Calculator- Butter Trinity OS
 cls
-color 0a
-title CALCULATOR- Butter Trinity OS
-ECHO * = MULTIPLY
-timeout /T 1 /NOBREAK >nul
-ECHO + = ADD
-timeout /T 1 /NOBREAK >nul
-ECHO _ = SUBTRACT
-timeout /T 1 /NOBREAK >nul
-ECHO / = DIVIDE
-timeout /T 1 /NOBREAK >nul
-ECHO % = Get Even or Odd
-timeout /T 1 /NOBREAK >nul
-SET /p UDefine= 
-SET /a UDefine=%UDefine%
-ECHO =
-ECHO %UDefine%
-ECHO.
+echo v2.0
+set /p first="Enter first number: "
+set /p second="Enter second number: "
+set /p opt="1-+ 2-- 3-* 4-/: "
+if %opt%==1 goto Plus
+if %opt%==2 goto Minus
+if %opt%==3 goto Multi
+if %opt%==4 goto Div
+echo Error
+
+
+set /a fin=%first%+%second%
+echo Sum is %fin%
 pause
 goto menu
+
+:Minus
+set /a fin=%first%-%second%
+echo Minus is %fin%
+goto calc
+
+:Multi
+set /a fin=%first%*%second%
+echo Multiple is %fin%
+goto calc
+
+:Div
+set /a fin=%first%/%second%
+echo Divide is %fin%
+goto calc
+
 :TEXT
 cls
 echo TEXT EDITOR 1.1
@@ -195,8 +265,9 @@ echo %WRITE% > %N%.txt
 goto HOME
 :Help
 cls
-echo Text editor is a programe made for creating text documents
+echo Text editor is a program made for creating text documents
 echo Calculator lets you process math problems
+echo BrokenMode is for breaking your computer (DANGEROUS)
 
 pause
 goto menu
@@ -266,10 +337,11 @@ if %SD% == 3 goto menu
 
 
 :menu2
-echo 1.calc 2.text editor
+echo 1.calc 2.text editor 3.ASCII paintings
 set /p OPTI= select option:
 if %OPTI% == 1 goto calc
 if %OPTI% == 2 goto UAC
+if %OPTI% == 3 goto ASCIIPaintings
 :UAC
 cls
 echo do you want to let this program to change your kernel?
@@ -322,4 +394,93 @@ CRASH_HANDLER
 echo Oh! we are taking you to the boot screen because you initiated a crash :) and we saved all your crash info to a file.
 timeout /T 2 /NOBREAK >nul
 goto boot
-
+:creds
+cls
+echo ======================
+timeout /T 1 /NOBREAK >nul
+echo       Credits      
+timeout /T 1 /NOBREAK >nul
+echo ======================
+timeout /T 1 /NOBREAK >nul
+echo  Based on: Purnjay OS
+timeout /T 1 /NOBREAK >nul
+echo made by: me, Purnjay P
+timeout /T 1 /NOBREAK >nul
+:ASCIIPaintings
+cls                                                                                                                                    `?g@q!               ,lA
+timeout /T 1 /NOBREAK >nul
+echo                                                                                                                                    \IXX]\    _nGT`      !8@Al
+echo                                                                                                                                 `+g@g+`   `1PRHWu`   \2OXn>  
+echo                                                                                                                              \lIXXI\    >IXHMU}`  `?pWg?`  
+timeout /T 1 /NOBREAK >nul  
+echo                                                                            \Zg^                                           ,]AWPpt`   `}bRHXC`   _IXX]\       
+echo                                                                            `l2,                                        _2A@qt,     ,2XHBX2`  `vg@q+`         
+echo                                                                                                                    ,vqWPI>       +UBBOl`   !A@q+`
+timeout /T 1 /NOBREAK >nul            
+echo                                                                                                                 _2gWA2_       \xmBBPc`  \]OXl,               
+echo                                                                                                              >]PWp1,         `PH&n"  \vpWgt`                 
+echo                                                                                                          `+8XXb?`             '!`  !pWP]\
+timeout /T 1 /NOBREAK >nul                    
+echo                                                                                                       `!IWOI!`                  ,lOXn\                       
+echo                   ,:                                                                              `t8XOn_                    >n@Al`                         
+echo                   ~&D>                                                                          ,vqWP]>   \uPGl      "aj   `1g@8!   
+timeout /T 1 /NOBREAK >nul                         
+echo                                                                                             `+8OO8+`    iXHHma~   `+UBHA`_nXp+`                              
+echo                                                                                          ,vqWPu>      ,0BBb+`    tGHBX]cg@q\                                 
+echo                                                                                       `?gWA2_        !PHH?     ^UBHWnfXX]\   
+timeout /T 1 /NOBREAK >nul                                
+echo                                                                                    ,vpWPl\         `SBBPc      ~0pqZ@g?`                                     
+echo                                                                                 _2AWg2_           }mHD2        `lPWn_                                        
+echo                                                                              >]PWqv,             xBHA>       >nWAl` 
+timeout /T 1 /NOBREAK >nul                                         
+echo                                                                          `+nOX8?`              `yBHd`     `vg@8!                                             
+echo                                                                       `?bXOI!`              "epWHHA`    `IXX2\                                               
+echo                                                                    \2gWqt,              ,20MHMgf},   `?g@I\     
+timeout /T 1 /NOBREAK >nul                                             
+echo                                                                 _]AWp1`          `~|`  }XHBPz-     \IXXy\                                                    
+echo          ^`                                                  >]PWq?,        `~__>>, `lGBMb"     `+hWg+`                                v&q                   
+echo         cBA                                              `!IOX8?`       `~___~`   ~u&HD$'     \2XXI\                                   ,i"                   
+echo          ^,                                           `+8XOn+`        ~!_`     `iqBH&u\     !q@g1` 
+timeout /T 1 /NOBREAK >nul                                                          
+echo                                                     >qXOI>      ^_____`    `!aAMHRPC'    ,lO&n_                                                              
+echo                                                  >IOX]_      '__,       |zh&HBm81:     \8@Al`                                                                
+echo                                              ,vpWA2\                |2PQHDAu+`      \2OXl,                                                                   
+echo                                           ,vpWA2\               `+wXBBGCt,        !q@g1`      
+timeout /T 1 /NOBREAK >nul                                                               
+echo                                        ,1pWA2\               '}yOHBP3>         \2OXn_    
+                                                                 
+echo                                     `?q@P2\       `,,,,\+lnUXBHRWGc         `+pWgv`                                     `^                                   
+echo                                   \8XO]_  '>     2MRRRRBHB@Pq2+,          _IXXI\                                        0B2                                  
+echo                                `1gWn>    -}`     \cllllc}~`            ,lA@q+`                                          `_` 
+timeout /T 1 /NOBREAK >nul                                    
+echo                              >nWPz\    ,__`              ``          _q@Al,                                                                                  
+echo                           `?g@8!    `~_>       `,|1lzTgAXDDA`     \2A@I_                                                                                     
+echo               `^        \n@g+`      ,    `>vnPMHHHRXXP$nn7!\   >uOXn>                                                                                        
+echo               0B2     `2MAlvy]2+,     |AX@HHDGbc+!\         >IOX8+`
+timeout /T 1 /NOBREAK >nul                                                                                             
+echo               `_`    ,gRXXXpI]ngWAt   -]nn5+,           `+nOX8+`                                                                                             
+echo                      pBM@v`     ,nR+                 _lqWPI!`                                                                                                
+echo                     ,WHHl         qA             ,?nPWql_                                                                                                    
+echo                      AHH2         pA       `>vIpOOg]+` 
+timeout /T 1 /NOBREAK >nul                                                                                                         
+echo                      >@HRI\     >pR2ttznqAOPPpIv_                                                                                                            
+echo MRRRR@XXXOAggggggggpIv+OHHRX@@MWPAqby]1+_,                                                                                                                   
+echo clllxnnnmHHHHHHXggP&BHMAAgq8Z8x+^                                                                                                                            
+echo         "nnnS&H@!   !nAXXXXX@RBHBWXXXXXPCv+!'`  
+timeout /T 1 /NOBREAK >nul                                                                                                                
+echo              }RHg`           ,^?nCnnCC$XBHHHB&Zat,                                              \Zg^                                                         
+echo               0HB1                      ^!!saAWBHDXXXXXXXmApy1_,                                `l2,                                                         
+echo               |@Hm                             _znnnnnnnn$P@RHBR@Pbei'                                                                                       
+echo                SHB,        !!`                             `:+le$P&BHRXACuv_ 
+timeout /T 1 /NOBREAK >nul                                                                                   
+echo                |MHz       ~mW>                                     ^rx0m&RHBmgZbuz1_                                                                         
+echo                 kHA                                                       \vnn0PXXRHR@OgCu7|`                                                                
+echo                 eH@\ '1llllll1>`                                                  ^czn0PXMHHDmh]|                                                            
+echo                 zHRAmDHBRRRRRHH@mA$l_`                                                   `"ra0XHBOql' 
+timeout /T 1 /NOBREAK >nul                                                          
+echo                 lHHBPn7',,,,,"an0GMHB@Pq2li',`                                                `+fg@HRGn\                                                     
+echo                 ,yz~              `jz0A@RBHBRMXXOAggqei,                                          `}nMHQgt`                                                  
+echo                                        `,\jlzxnn$UgAWBHMACc^                                         'lAMHOc`                                                
+echo                                                      |zpXBHB0\                                          ,pDH@n_  
+pause
+goto menu                                            
